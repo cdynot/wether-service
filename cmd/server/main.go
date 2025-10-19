f7e2310dfcd2acfaa3210df984dea76045344780
Anton Novikov <antony2113@mail.ru>
2025-10-19 14:32:01 +0300

    add two http client

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"sync"
 	"time"
 
+	"github.com/cdynot/wether-service/internal/client/http/geocoding"
+	"github.com/cdynot/wether-service/internal/client/http/open_meteo"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-co-op/gocron/v2"
@@ -17,12 +20,32 @@ var httpPort = ":3000"
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	httpClient := &http.Client{
+		Timeout: time.Second * 10,
+	}
+	geocodingClient := geocoding.NewClient(httpClient)
+	openMeteoClient := open_meteo.NewClient(httpClient)
+
 	r.Get("/{city}", func(w http.ResponseWriter, r *http.Request) {
 		city := chi.URLParam(r, "city")
 
-		fmt.Println("request city,", city)
+		geoRes, err := geocodingClient.GetCoords(city)
+		if err != nil {
+			log.Println(err)
+		}
 
-		_, err := w.Write([]byte("welcome"))
+		openMetRes, err := openMeteoClient.GetTemperature(geoRes.Latitude, geoRes.Longitude)
+		if err != nil {
+			log.Println(err)
+		}
+
+		raw, err := json.Marshal(openMetRes)
+		if err != nil {
+			log.Println(err)
+		}
+
+		_, err = w.Write(raw)
 		if err != nil {
 			log.Println(err)
 		}
---
+++ internal/client/http/open_meteo/client.go
@@ -0,0 +1,49 @@
+package open_meteo
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Response struct {
+	Current struct {
+		Time        string  `json:"time"`
+		Temperature float64 `json:"temperature_2m"`
+	}
+}
+
+type client struct {
+	httpClient *http.Client
+}
+
+func NewClient(httpClient *http.Client) *client {
+	return &client{httpClient: httpClient}
+}
+
+func (c *client) GetTemperature(lat, long float64) (Response, error) {
+	res, err := c.httpClient.Get(
+		fmt.Sprintf(
+			"https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&current=temperature_2m",
+			lat,
+			long,
+		),
+	)
+	defer res.Body.Close()
+
+	if err != nil {
+		return Response{}, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return Response{}, fmt.Errorf("status cpde %d", res.Status)
+	}
+
+	var result Response
+	err = json.NewDecoder(res.Body).Decode(&result)
+	if err != nil {
+		return Response{}, err
+	}
+
+	return result, nil
+}
